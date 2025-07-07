00cbbfae66e70a2d15dd2aff1384a4f612322149
Akash Sasikumar <vava73066@gmail.com>
2025-07-07 14:35:48 +0530

    Update MQTT_LED.ino

--- MQTT_LED.ino
+++ MQTT_LED.ino
@@ -1,70 +1,82 @@
 #include <WiFi.h>
-#include <PubSubClient.h>
+#include <PicoMQTT.h>
 
-const char* ssid = "GalaxyA";
-const char* password = "xyeb0000";
+#define WIFI_SSID "Luminar_Ruckus4"  
+#define WIFI_PASSWORD "space_bar"  
+#define LED_PIN 2                    
 
-const char* mqtt_server = "broker.hivemq.com";
-const int mqtt_port = 1883;
-const char* mqtt_topic = "esp32/led";
+class MyMQTTServer : public PicoMQTT::Server {
+protected:
 
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-#define LED_PIN 2
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  String message = "";
-  for (int i = 0; i < length; i++) {
-    message += (char)payload[i];
-  }
-
-  Serial.print("Received: ");
-  Serial.println(message);
-
-  if (message == "LED ON") {
-    digitalWrite(LED_PIN, HIGH);
-  } else if (message == "LED OFF") {
-    digitalWrite(LED_PIN, LOW);
-  }
-}
-
-void reconnect() {
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    if (client.connect("ESP32Client")) {
-      Serial.println("connected");
-      client.subscribe(mqtt_topic);
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" trying again in 5 seconds");
-      delay(5000);
+  PicoMQTT::ConnectReturnCode auth(const char *client_id, const char *username, const char *password) override {
+    Serial.println("auth() called");
+    if (!username || !password) {
+      return PicoMQTT::CRC_NOT_AUTHORIZED;
     }
+
+    if ((String(username) == "alice" && String(password) == "secret") ||
+        (String(username) == "bob" && String(password) == "password")) {
+      Serial.println("MQTT Auth accepted");
+      return PicoMQTT::CRC_ACCEPTED;
+    }
+
+    Serial.println("MQTT Auth rejected");
+    return PicoMQTT::CRC_BAD_USERNAME_OR_PASSWORD;
   }
-}
+};
+
+MyMQTTServer mqtt;
 
 void setup() {
+  Serial.begin(115200);
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, LOW);
 
-  Serial.begin(115200);
-  WiFi.begin(ssid, password);
+  Serial.printf("Connecting to WiFi: %s\n", WIFI_SSID);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-  Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    delay(1000);
     Serial.print(".");
   }
-  Serial.println("\nWiFi connected");
 
-  client.setServer(mqtt_server, mqtt_port);
-  client.setCallback(callback);
+  Serial.printf("\nWiFi connected, IP: %s\n", WiFi.localIP().toString().c_str());
+
+  mqtt.begin();
+  Serial.println("MQTT server started");
+
+  mqtt.subscribe("led", [](char *topic_ptr, PicoMQTT::IncomingPacket& packet) {
+    Serial.println("Lambda handleMqttMessage called"); 
+
+    String topic = String(topic_ptr);
+
+
+    size_t payload_len = packet.get_remaining_size();
+    char payload_buffer[payload_len + 1]; 
+    packet.read((uint8_t*)payload_buffer, payload_len);
+    payload_buffer[payload_len] = '\0';
+
+    String msg = String(payload_buffer); 
+
+    Serial.printf("Received on topic [%s]: %s\n", topic.c_str(), msg.c_str());
+
+    if (topic == "led") {
+      if (msg == "on") {
+        digitalWrite(LED_PIN, HIGH);
+        Serial.println("LED turned ON");
+      } else if (msg == "off") {
+        digitalWrite(LED_PIN, LOW);
+        Serial.println("LED turned OFF");
+      } else {
+        Serial.printf("Unknown command for LED: %s\n", msg.c_str());
+      }
+    }
+  });
+
+  Serial.println("MQTT server subscribed to 'led' topic with lambda callback");
 }
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
+  mqtt.loop();
 }
